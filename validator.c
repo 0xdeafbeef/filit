22b101151b816e78789bcb6198b2d122462816d7
adminvasheypomoiki <kek@cheburek.lol>
2019-01-27 18:22:47 +0300

    working validation

--- validator.c
+++ validator.c
@@ -6,7 +6,7 @@
 /*   By: qhetting <qhetting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/16 21:55:09 by qhetting          #+#    #+#             */
-/*   Updated: 2019/01/27 16:17:09 by qhetting         ###   ########.fr       */
+/*   Updated: 2019/01/27 18:19:53 by qhetting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,6 +74,217 @@ int pre_parse(int fd, char **tetraminos)
 //}
 
 
+
+int _x(int pos)
+{
+	return (pos % 4);
+}
+
+int _y(int pos)
+{
+	return (pos / 4);
+}
+
+/*
+_y(pos) > 0 && _y(pos) < 4 && _x(pos) > 0 && _x(pos) < 4
+ */
+int check_1(char *tetr, int pos)
+{
+	int sharps;
+
+	sharps = 0;
+
+	if (tetr[pos + UP] == '#')
+		sharps++;
+	if (tetr[pos + 1] == '#')
+		sharps++;
+	if (tetr[pos + DOWN] == '#')
+		sharps++;
+	if (tetr[pos - 1] == '#')
+		sharps++;
+	return (sharps);
+}
+
+//x ==0
+int check_2(char *tetr, int pos)
+{
+	int sharps;
+
+	sharps = 0;
+
+	if (tetr[pos + UP] == '#')
+		sharps++;
+	if (tetr[pos + 1] == '#')
+		sharps++;
+	if (tetr[pos + DOWN] == '#')
+		sharps++;
+	return (sharps);
+}
+
+int check_3(char *tetr, int pos)
+{
+	int sharps;
+
+	sharps = 0;
+
+	if (tetr[pos + UP] == '#')
+		sharps++;
+	if (tetr[pos + DOWN] == '#')
+		sharps++;
+	if (tetr[pos - 1] == '#')
+		sharps++;
+	return (sharps);
+}
+
+int check_4(char *tetr, int pos)
+{
+	int sharps;
+
+	sharps = 0;
+
+	if (tetr[pos + 1] == '#')
+		sharps++;
+	if (tetr[pos + DOWN] == '#')
+		sharps++;
+	if (tetr[pos - 1] == '#')
+		sharps++;
+	return (sharps);
+}
+
+int check_5(char *tetr, int pos)
+{
+	int sharps;
+
+	sharps = 0;
+
+	if (tetr[pos + 1] == '#')
+		sharps++;
+	if (tetr[pos + DOWN] == '#')
+		sharps++;
+	return (sharps);
+}
+
+int check_6(char *tetr, int pos)
+{
+	int sharps;
+
+	sharps = 0;
+
+	if (tetr[pos + DOWN] == '#')
+		sharps++;
+	if (tetr[pos - 1] == '#')
+		sharps++;
+	return (sharps);
+}
+
+int check_7(char *tetr, int pos)
+{
+	int sharps;
+
+	sharps = 0;
+
+	if (tetr[pos + UP] == '#')
+		sharps++;
+	if (tetr[pos + 1] == '#')
+		sharps++;
+	if (tetr[pos - 1] == '#')
+		sharps++;
+	return (sharps);
+}
+
+int check_8(char *tetr, int pos)
+{
+	int sharps;
+
+	sharps = 0;
+	if (tetr[pos + UP] == '#')
+		sharps++;
+	if (tetr[pos + 1] == '#')
+		sharps++;
+	return (sharps);
+}
+
+int check_9(char *tetr, int pos)
+{
+	int sharps;
+
+	sharps = 0;
+	if (tetr[pos + UP] == '#')
+		sharps++;
+	if (tetr[pos - 1] == '#')
+		sharps++;
+	return (sharps);
+}
+
+int get_flag(int pos, char *tetra)
+{
+	int flag;
+	if (_y(pos) > 0 && _y(pos) < 4)
+	{
+		if (_x(pos) > 0 && _x(pos) < 4)
+			flag = check_1(tetra, pos);
+		else if (_x(pos) == 0)
+			flag = check_2(tetra, pos);
+		else
+			flag = check_3(tetra, pos);
+	} else if (_y(pos) == 0)
+	{
+		if (_x(pos) > 0 && _x(pos) < 4)
+			flag = check_4(tetra, pos);
+		else if (_x(pos) == 0)
+			flag = check_5(tetra, pos);
+		else
+			flag = check_6(tetra, pos);
+	} else
+	{
+		if (_x(pos) > 0 && _x(pos) < 4)
+			flag = check_7(tetra, pos);
+		else if (_x(pos) == 0)
+			flag = check_8(tetra, pos);
+		else
+			flag = check_9(tetra, pos);
+	}
+	return (flag);
+}
+
+int validate_tetra(char **tetramino)
+{
+	int pos;
+	int sharp_count;
+	char *tetra;
+	int flag;
+
+	flag = 0;
+	tetra = *tetramino;
+	pos = 0;
+	sharp_count = pos;
+	while (pos < 16)
+	{
+		if (tetra[pos] == '#' && sharp_count < 4)
+			flag += get_flag(pos, tetra);
+		pos++;
+	}
+	return ((flag >= 6 && flag <= 8) ? 1 : 0);
+}
+
+int validate(char **tetra)
+{
+	int i;
+	char *cut;
+	i = 0;
+
+	cut = malloc(17);
+	cut[16] = 0;
+	while (i != ft_strlen(*tetra))
+	{
+		ft_memcpy(cut, (i + *(tetra)), 16);
+		if (!validate_tetra(&cut))
+			return 0;
+		i += 16;
+	}
+	free(cut);
+	return (1);
+}
 
 char **generate_arrays(char **tet_inpt)
 {
