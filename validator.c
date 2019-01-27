d5902ed7408fb8617b5dfec024d8bc748a70c569
adminvasheypomoiki <kek@cheburek.lol>
2019-01-27 16:19:01 +0300

    rewrite_all

--- validator.c
+++ validator.c
@@ -6,7 +6,7 @@
 /*   By: qhetting <qhetting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/16 21:55:09 by qhetting          #+#    #+#             */
-/*   Updated: 2019/01/24 20:40:05 by qhetting         ###   ########.fr       */
+/*   Updated: 2019/01/27 16:17:09 by qhetting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,111 +73,6 @@ int pre_parse(int fd, char **tetraminos)
 //	ft_strcpy(valid[18], "##...##");
 //}
 
-
-
-int _x(int pos)
-{
-	return (pos % 4);
-}
-
-int _y(int pos)
-{
-	return (pos / 4);
-}
-/*
-_y(pos) > 0 && _y(pos) < 4 && _x(pos) > 0 && _x(pos) < 4
- */
-int check_1(char *tetr, int pos)
-{
-
-	return 1488;
-}
-
-int check_2(char *tetr, int pos)
-{}
-
-int check_3(char *tetr, int pos)
-{}
-
-int check_4(char *tetr, int pos)
-{}
-// [0][0]
-int check_5(char *tetr, int pos)
-{
-	//check right oriented
-	if (tetr[pos+1]==043 )
-	{
-		if (tetr[pos+1+DOWN])
-		{
-			if(tetr[])
-		}
-	}
-
-}
-
-int check_6(char *tetr, int pos)
-{}
-
-int check_7(char *tetr, int pos)
-{}
-
-int check_8(char *tetr, int pos)
-{}
-
-int check_9(char *tetr, int pos)
-{}
-
-int get_flag(int pos, char *tetra)
-{
-	int flag;
-	if (_y(pos) > 0 && _y(pos) < 4)
-	{
-		if (_x(pos) > 0 && _x(pos) < 4)
-			flag = check_1(tetra, pos);
-		else if (_x(pos) == 0)
-			flag = check_2(tetra, pos);
-		else
-			flag = check_3(tetra, pos);
-	} else if (_y(pos) == 0)
-	{
-		if (_x(pos) > 0 && _x(pos) < 4)
-			flag = check_4(tetra, pos);
-		else if (_x(pos) == 0)
-			flag = check_5(tetra, pos);
-		else
-			flag = check_6(tetra, pos);
-	} else
-	{
-		if (_x(pos) > 0 && _x(pos) < 4)
-			flag = check_7(tetra, pos);
-		else if (_x(pos) == 0)
-			flag = check_8(tetra, pos);
-		else
-			flag = check_9(tetra, pos);
-	}
-	return flag;
-}
-
-//todo check IF Y > 0   IF Y < 4  Y > O && Y < 4
-int validate_tetra(char **tetramino)
-{
-	int pos;
-	int sharp_count;
-	char *tetra;
-	int flag;
-
-	flag = -1;
-	tetra = *tetramino;
-	pos = 0;
-	sharp_count = pos;
-	while (pos < 16)
-	{
-		if (tetra[pos] == '#' && sharp_count < 4)
-			flag = get_flag(pos, tetra);
-		pos++;
-	}
-	return (flag);
-}
 
 
 char **generate_arrays(char **tet_inpt)
